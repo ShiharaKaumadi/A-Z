59011849068248bde3683f5bb5a95bf54b8cbc7a
ShiharaKaumadi <eshani9544@gmail.com>
2024-04-03 21:41:12 +0530

    Familiarize with array element iteration

---
+++ 14 JS Arrays/arrayElementIteration.js
@@ -0,0 +1,73 @@
+//for  loop
+const fruits = ["mango","banana","apple","grapes","orange","pine apple","strawberry","blue berry","water melon"];
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(i + " " + fruits[i]);
+}
+/*
+0 mango
+1 banana
+2 apple
+3 grapes
+4 orange
+5 pine apple
+6 strawberry
+7 blue berry
+8 water melon
+*/
+
+//for each loop
+const array = ["one","two",["three","four","five"],2,true,12.05,{"name":"William","age":12,"gender":"male"},true,{"friendList":["James","Mandela","Philips","Revon"]},{name : "Timmy",age : 1}];
+array.forEach((element)=> console.log(element));
+/*one
+two
+[ 'three', 'four', 'five' ]
+2
+true
+12.05
+{ name: 'William', age: 12, gender: 'male' }
+true
+{ friendList: [ 'James', 'Mandela', 'Philips', 'Revon' ] }
+{ name: 'Timmy', age: 1 }   */
+array.forEach(function indexBased(i){
+    console.log(i )
+});
+/*one
+two
+[ 'three', 'four', 'five' ]
+2
+true
+12.05
+{ name: 'William', age: 12, gender: 'male' }
+true
+{ friendList: [ 'James', 'Mandela', 'Philips', 'Revon' ] }
+{ name: 'Timmy', age: 1 }
+one
+two
+[ 'three', 'four', 'five' ]
+2
+true
+12.05
+{ name: 'William', age: 12, gender: 'male' }
+true
+{ friendList: [ 'James', 'Mandela', 'Philips', 'Revon' ] }
+{ name: 'Timmy', age: 1 }
+
+*/
+
+//foreach loop with array with objects
+const friendDetailArray = [
+    {name:"John",age:25,job:"Executive Officer"},
+    {name:"Ferdinan",age:29,job:"Trainee Clerk"},
+    {name:"Rose",age:23,job:"Software Engineer"},
+    {name:"Xio Me",age:26,job:"Architect"},
+    {name:"Cha Joon Tak",age:24,job:"Fashion Designer"}
+];
+friendDetailArray.forEach((friend)=>{console.log(friend)});//👇 output
+/*
+{ name: 'John', age: 25, job: 'Executive Officer' }
+{ name: 'Ferdinan', age: 29, job: 'Trainee Clerk' }
+{ name: 'Rose', age: 23, job: 'Software Engineer' }
+{ name: 'Xio Me', age: 26, job: 'Architect' }
+{ name: 'Cha Joon Tak', age: 24, job: 'Fashion Designer' }
+*/
